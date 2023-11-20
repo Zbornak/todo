2c90155ac9a6294e2b2a5492bd4f3676a7b3c9d5
zbornak <zbornak@protonmail.com>
2023-11-20 15:00:34 +0000

    Wrote basic CLI

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -7,9 +7,32 @@ Date: 20/11/2023
 package main
 
 import (
+	"cli/todo"
 	"fmt"
+	"os"
+	"strings"
 )
 
+const todoFilename = ".todo.json"
+
 func main() {
-	fmt.Println("Hello world")
+	l := &todo.List{}
+	if err := l.Get(todoFilename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+		if err := l.Save(todoFilename); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
 }
